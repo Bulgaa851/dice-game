52df29ebafca7b89c9283bcb66727e374ff6d133
Bulgaa <bbulgaa211@gmail.com>
2022-06-05 17:21:47 +0800

    new game nemeh

--- app.js
+++ app.js
@@ -1,61 +1,59 @@
-//  Toglogchiin eelj solih func
-// neg dugeer togloch 0 hoer dugaar togloch 1
-var activePlayer = 0;
+///Хувьсагчид
+var activePlayer = 0; //Тоглогч 1-р тоглогч=0 2-р тоглогч = 1
+var CollectedScore = [0, 0]; //Тоглогчийн цуглуулсан оноо
+var roundScore = 0; //Тоглогчийн нэг үед цуглуулсан оноо
+//Тоглогч солих
 function playerChange() {
   roundScore = 0;
   document.getElementById("current-" + activePlayer).textContent = roundScore;
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
-  diceDom.style.display = "none";
 }
+//Шинэ -ээр эхлүүлэх function
+function newGame() {
+  activePlayer = 0;
+  CollectedScore = [0, 0];
+  roundScore = 0;
 
-//  Tsugluulsan ono hadgalah func
-var CollectedScore = [0, 0];
-//  Toglogchiiin eeljind tsugluusan onoo ni func
-
-var roundScore = 0;
-
-//  Shooni ali talaara buusniih hadgalah 1-6 fun
-
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  //Шооны зураг алга болгох
+  document.querySelector(".dice").style.display = "none";
+  //Player 1 -ээс эхлүүлэх
+  document.querySelector(".player-0-panel").classList.add("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  //Нэр тохируулалт
+  document.getElementById("name-0").textContent = "PLAYER 1";
+  document.getElementById("name-1").textContent = "PLAYER 2";
+  //Winner class арилгах
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+}
+//Шооны өгөгдөл
 var diceNumber = Math.floor(Math.random() * 6) + 1;
-
-// DOM  ~ Document txt contnet
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-
-// Dice image class
-
-document.querySelector(".dice").style.display = "none";
-
-// diceDOm
-
 var diceDom = document.querySelector(".dice");
-
-// Click event and Dice Image
-
-// --------------Roll Dice ----------------------//
+diceDom.style.display = "none";
+//Roll дархад явагдах event
 document.querySelector(".btn-roll").addEventListener("click", function () {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   diceDom.style.display = "block";
   diceDom.src = "dice-" + diceNumber + ".png";
-
   if (diceNumber !== 1) {
     roundScore = roundScore + diceNumber;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
     playerChange();
+    diceDom.src = "dice-1.png";
   }
 });
-//----------------------------------------------------------HOLD---------------------------------//
+//Hold дархад явагдах event
 document.querySelector(".btn-hold").addEventListener("click", function () {
   CollectedScore[activePlayer] = CollectedScore[activePlayer] + roundScore;
   document.getElementById("score-" + activePlayer).textContent =
     CollectedScore[activePlayer];
-  ///hojson eshiig shalgah
-
   if (CollectedScore[activePlayer] > 100) {
     document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
     document
@@ -66,9 +64,10 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
       .classList.remove("active");
   } else {
     playerChange();
+    diceDom.style.display = "none";
   }
 });
-
-//New game
-
-document.querySelector(".btm");
+//New Game дархад явагдах event
+document.querySelector(".btn-new").addEventListener("click", function () {
+  newGame();
+});
